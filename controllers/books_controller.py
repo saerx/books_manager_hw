767ac78dd118f029287d78f778f531ba498978d4
saerx <saerlaith.robyn@gmail.com>
2020-11-25 20:25:59 +0000

    added add book function

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Blueprint, redirect
+from flask import Flask, render_template, Blueprint, redirect, request
 from models.book import Book
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
@@ -18,11 +18,28 @@ def books():
 
 # NEW
 # GET '/books/new'
-
+@books_blueprint.route("/books/new", methods = ["GET"])
+def new_book():
+    authors = author_repository.select_all()
+    return render_template("books/new.html", all_authors=authors)
 
 # CREATE
 # POST '/books'
+@books_blueprint.route("/books", methods=["POST"])
+def create_book():
+    #Grab the form data for description, user_id, duration and completed
+    title = request.form['title']
+    genre = request.form['genre']
+    publisher = request.form['publisher']
+    author_id = request.form['author_id']
+    #select the user using the repository 
+    author = author_repository.select(author_id)
+    #create a new task object
+    book = Book(title, genre, publisher, author)
+    #save that task object back to the database
+    book_repository.save(book)
 
+    return redirect('/books')
 
 # SHOW
 # GET '/books/<id>'
